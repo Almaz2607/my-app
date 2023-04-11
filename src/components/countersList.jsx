e5c07687297206c56bb1d710abe715db29d1e418
Almaz Kubakaev <almazkubakaev2607@gmail.com>
2023-04-11 09:43:59 +0600

    composition of components,solution task-1

--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -20,10 +20,30 @@ const CountersList = () => {
     setCounters(initialState);
   };
 
+  const handleIncrement = (id) => {
+    const countIncrement = counters.map((count) =>
+      count.id === id ? { ...count, value: (count.value += 1) } : count
+    );
+    setCounters(countIncrement);
+  };
+
+  const handleDecrement = (id) => {
+    const countDecrement = counters.map((count) =>
+      count.id === id ? { ...count, value: (count.value -= 1) } : count
+    );
+    setCounters(countDecrement);
+  };
+
   return (
     <>
       {counters.map((count) => (
-        <Counter key={count.id} {...count} onDelete={handleDelete} />
+        <Counter
+          key={count.id}
+          {...count}
+          onDelete={handleDelete}
+          onIncrement={handleIncrement}
+          onDecrement={handleDecrement}
+        />
       ))}
       <button className="btn btn-primary btn-sm m-2" onClick={handleReset}>
         Reset
